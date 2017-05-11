c69dadaf41d9ab30cdbdd21a22e1eff0ff9aa6b0
Carlos Hernández <chernandezgil@gmail.com>
2017-05-11 19:49:53 +0200

    another addition of user2

--- app/src/main/java/com/example/gitworkflow/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gitworkflow/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //addition of user2
+        //another addition of user2
     }
 }
